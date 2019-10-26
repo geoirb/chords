d4c774f0da444fc257a8d42d5dac1b67d65689c9
GeoIrb <morochev.g@gmail.com>
2019-10-27 01:31:33 +0300

    response json

--- models/chord.go
+++ models/chord.go
@@ -1,45 +1,49 @@
 package models
 
 import (
-	"fmt"
 	"strings"
 )
 
 type Chord struct {
-	Symbol string
-	Line   int
-	Index  int
+	Symbol string `json:"chord"`
+	Index  int    `json:"position"`
 }
 
-func getChords(i int, line string) (lineChords []Chord) {
-	fmt.Println(line)
-	resultLine := line
+type Line struct {
+	CleanLine string  `json:"line"`
+	Сhords    []Chord `json:"chords"`
+	Index     int     `json:"idx"`
+}
+
+func getChords(line string) (resultLine string, lineChords []Chord) {
+	resultLine = line
 
 	for begin, s := range line {
 		if s == '[' {
 			end := strings.Index(line[begin:], "]")
 			chord := Chord{
 				Symbol: line[begin+1 : begin+end],
-				Line:   i,
 				Index:  begin + 1,
 			}
 
-			fmt.Println(chord)
-
-			// lineChords = append(lineChords, chord)
+			lineChords = append(lineChords, chord)
 			resultLine = strings.Replace(resultLine, line[begin:begin+end+1], "", 1)
 		}
 	}
 
-	fmt.Println(resultLine)
-
 	return
 }
 
-func CreateText(text string) {
+func CreateJSON(text string) (textJSON []Line) {
 	lines := strings.Split(text, "\n")
 	for i, line := range lines {
+		lineJSON := Line{
+			Index: i,
+		}
 		line = strings.TrimSpace(line)
-		getChords(i, line)
+		lineJSON.CleanLine, lineJSON.Сhords = getChords(line)
+		textJSON = append(textJSON, lineJSON)
 	}
+
+	return
 }
