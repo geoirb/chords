b9e0e0151bd087baada67b7dd68041adf894cc95
GeoIrb <morochev.g@gmail.com>
2019-10-27 00:53:54 +0300

    очищаю текст и ищу все аккорды

--- models/chord.go
+++ models/chord.go
@@ -11,18 +11,29 @@ type Chord struct {
 	Index  int
 }
 
-func getChords(i int, line string) {
+func getChords(i int, line string) (lineChords []Chord) {
 	fmt.Println(line)
 	resultLine := line
+
 	for begin, s := range line {
 		if s == '[' {
-			end := strings.Index(line[begin+1:], "]")
-			fmt.Printf("%c %c\n", line[begin], line[end])
-			resultLine = resultLine[:begin] + resultLine[end+1:]
+			end := strings.Index(line[begin:], "]")
+			chord := Chord{
+				Symbol: line[begin+1 : begin+end],
+				Line:   i,
+				Index:  begin + 1,
+			}
+
+			fmt.Println(chord)
+
+			// lineChords = append(lineChords, chord)
+			resultLine = strings.Replace(resultLine, line[begin:begin+end+1], "", 1)
 		}
 	}
 
-	// fmt.Println(resultLine)
+	fmt.Println(resultLine)
+
+	return
 }
 
 func CreateText(text string) {
